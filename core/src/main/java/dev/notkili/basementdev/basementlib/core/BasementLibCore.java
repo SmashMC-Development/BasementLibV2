127a88596b1ba8db670312abdf5a62f9aaaa608d
NotKili <familie_kessler.kilian@t-online.de>
2024-08-28 02:42:45 +0200

    version(core): 1.0.0

--- core/src/main/java/dev/notkili/basementdev/basementlib/core/BasementLibCore.java
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/BasementLibCore.java
@@ -2,8 +2,14 @@ package dev.notkili.basementdev.basementlib.core;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("basementlib_core")
+@Mod(BasementLibCore.MOD_ID)
 public class BasementLibCore {
+    public static final String MOD_ID = "basementlib_core";
+    
+    public static final String MOD_NAME = "BasementLib Core";
+    
+    public static final String MOD_VERSION = "1.0.0";
+    
     public BasementLibCore() {
     }
 }
