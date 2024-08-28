dc60aa365338b94ba1c0c80374b232e017da7def
NotKili <familie_kessler.kilian@t-online.de>
2024-08-28 02:17:04 +0200

    feat: added default base project structure for basementlib:core mod

---
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/BasementLibCore.java
@@ -0,0 +1,9 @@
+package dev.notkili.basementdev.basementlib.core;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("basementlib_core")
+public class BasementLibCore {
+    public BasementLibCore() {
+    }
+}
