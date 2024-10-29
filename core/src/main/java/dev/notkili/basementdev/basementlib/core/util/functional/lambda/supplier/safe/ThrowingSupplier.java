5218f8d4e1f1ace4a66b91ea9290f19ee818b0bf
NotKili <familie_kessler.kilian@t-online.de>
2024-10-29 08:14:32 +0100

    docs(core/misc/functional): Added javadoc comments to Supplier & ThrowingSupplier

--- core/src/main/java/dev/notkili/basementdev/basementlib/core/util/functional/lambda/supplier/safe/ThrowingSupplier.java
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/util/functional/lambda/supplier/safe/ThrowingSupplier.java
@@ -1,6 +1,26 @@
 package dev.notkili.basementdev.basementlib.core.util.functional.lambda.supplier.safe;
 
+import dev.notkili.basementdev.basementlib.core.util.functional.lambda.supplier.unsafe.Supplier;
+
+/**
+ * Represents a supplier of results. <br>
+ * Unlike {@link Supplier}, this function can throw a checked exception.
+ *
+ * <p>There is no requirement that a new or distinct result be returned each
+ * time the supplier is invoked.
+ * @param <T> the type of results supplied by this supplier
+ * @param <E> the type of exception that can be thrown
+ * @author NotKili
+ * @since 1.0.0
+ */
 @FunctionalInterface
 public interface ThrowingSupplier<T, E extends Exception> {
+    /**
+     * Computes a result.
+     * @return The result of the computation
+     * @throws E The exception that can be thrown while computing the result
+     * @author NotKili
+     * @since 1.0.0
+     */
     T get() throws E;
 }
--- core/src/main/java/dev/notkili/basementdev/basementlib/core/util/functional/lambda/supplier/unsafe/Supplier.java
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/util/functional/lambda/supplier/unsafe/Supplier.java
@@ -1,6 +1,22 @@
 package dev.notkili.basementdev.basementlib.core.util.functional.lambda.supplier.unsafe;
 
+/**
+ * Represents a supplier of results. <br>
+ * Unlike {@link Supplier}, this function can not throw a checked exception.
+ * 
+ * <p>There is no requirement that a new or distinct result be returned each
+ * time the supplier is invoked.
+ * @param <T> the type of results supplied by this supplier
+ * @author NotKili
+ * @since 1.0.0
+ */
 @FunctionalInterface
 public interface Supplier<T> {
+    /**
+     * Computes a result.
+     * @return The result of the computation
+     * @author NotKili
+     * @since 1.0.0
+     */
     T get();
 }
