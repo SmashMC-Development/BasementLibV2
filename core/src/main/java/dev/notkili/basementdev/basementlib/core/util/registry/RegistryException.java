795eb7bc15582025cab5ae4ee456a47f36a97c15
NotKili <familie_kessler.kilian@t-online.de>
2024-11-02 15:11:16 +0100

    docs(core/util/registry): RegistryException

--- core/src/main/java/dev/notkili/basementdev/basementlib/core/util/registry/RegistryException.java
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/util/registry/RegistryException.java
@@ -1,5 +1,11 @@
 package dev.notkili.basementdev.basementlib.core.util.registry;
 
+/**
+ * An exception that is thrown when a registry encounters any error. <br>
+ * The exception message will contain the registry name and the message that was passed to the constructor.
+ * @author NotKili
+ * @since 1.0.0
+ */
 public class RegistryException extends RuntimeException {
     public RegistryException(IRegistry<?, ?> registry, String message) {
         super("[Registry: " + registry.getName() + "] threw an exception: " + message);
