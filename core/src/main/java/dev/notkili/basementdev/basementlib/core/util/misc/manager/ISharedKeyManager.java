c21b74b7b888efb8a3c8ca584d28ece9ba9f8820
NotKili <familie_kessler.kilian@t-online.de>
2024-11-03 02:29:12 +0100

    feat(core/util/misc/manager): ISharedKeyManager
    Specialization of IManager that can work with a singular Key type for the registry and the factory instead of 2 (basic implementation)
    
    Signed-off-by: NotKili <familie_kessler.kilian@t-online.de>

---
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/util/misc/manager/ISharedKeyManager.java
@@ -0,0 +1,6 @@
+package dev.notkili.basementdev.basementlib.core.util.misc.manager;
+
+import dev.notkili.basementdev.basementlib.core.util.misc.key.IKey;
+
+public interface ISharedKeyManager<K extends IKey, V> extends IManager<K, K, V> {
+}
