fd6eaa208a1183ac29440494d1f96da196e818a1
NotKili <familie_kessler.kilian@t-online.de>
2024-11-03 02:28:43 +0100

    feat(core/util/misc/manager): IGenericManager
    Specialization of IManager that can work with any type of IKey instead of a singular one
    
    Signed-off-by: NotKili <familie_kessler.kilian@t-online.de>

---
+++ core/src/main/java/dev/notkili/basementdev/basementlib/core/util/misc/manager/IGenericManager.java
@@ -0,0 +1,6 @@
+package dev.notkili.basementdev.basementlib.core.util.misc.manager;
+
+import dev.notkili.basementdev.basementlib.core.util.misc.key.IKey;
+
+public interface IGenericManager<V> extends IManager<IKey, IKey, V> {
+}
